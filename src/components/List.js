8ea5ea0b1273618c199c6ef825d0b502c97aa6bd
Brian OLeary <oleary.brian1@gmail.com>
2020-10-30 15:49:10 -0400

    new branch

--- src/components/List.js
+++ src/components/List.js
@@ -1,10 +1,21 @@
 import React from "react";
 import BootstrapTable from 'react-bootstrap-table-next';
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+import ToolkitProvider, { Search } from 'react-bootstrap-table2-toolkit';
 import Moment from 'react-moment';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const { SearchBar } = Search;
+
+
+function imageFormat(cell, row) {
+  console.log(cell , row)
+  return `<img src= "https://randomuser.me/api/portraits/thumb/men/83.jpg" /> ${cell}`; 
+  };
 
 const columns = [
   {
+    dataFormat: {imageFormat},
     dataField: 'image',
     text: 'Image',
   },
@@ -12,7 +23,6 @@ const columns = [
     dataField: 'name',
     text: 'Name',
     sort: true,
-
   },
   {
     dataField: 'phone',
@@ -28,34 +38,45 @@ const columns = [
   },];
 
 const employeeArray = [];
+let searchValue = "";
 const defaultSorted = [{
   dataField: 'email', // if dataField is not match to any column you defined, it will be ignored.
   order: 'asc' // desc or asc
 }];
 
+
+
+function dateFormat(date) {
+return date.substring(5, 6) + "/" + date.substring(8, 9) + "/" + date.substring(0, 4); 
+};
 // Whenever we try to render an array containing JSX, React knows to render each JSX element separately
 function List(props) {
   {
     props.employees.map(item => employeeArray.push(
       {
-        image: "<image src=" + item.picture.thumbnail + "/>,",
+        image: item.picture.thumbnail,
+        // image: `<img src= "${item.picture.thumbnail}" />,`,
         name: item.name.first + " " + item.name.last,
         phone: item.phone,
         email: item.email,
-        birthdate: item.dob.date
-            },
+        birthdate: dateFormat(item.dob.date)
+      },
     ))
   }
 
   return (
-
-    <BootstrapTable
-      keyField='email'
-      data={employeeArray}
-      columns={columns}
-      defaultSorted={ defaultSorted } 
-      striped />
+    <div>
+      <h3>Input something at below input field:</h3>
+      <img src= "https://randomuser.me/api/portraits/thumb/men/83.jpg" />,
+      <SearchBar {...props.searchProps} />
+      <BootstrapTable
+        keyField='email'
+        data={employeeArray}
+        columns={columns}
+        defaultSorted={defaultSorted}
+        striped />
+    </div>
   );
-}
+};
 
 export default List;
